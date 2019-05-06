3155db5e7ad2668b73c1ce6c2a7931e8e8188b9e
Matt Whitby <whitbymgt@gmail.com>
2019-05-06 13:56:28 +0100

    Created initial maven configuration
    Includes dependency for jsoup for later use

---
+++ sainsburys-scraper/src/main/java/com/scaper/app/ScraperApplication.java
@@ -0,0 +1,12 @@
+package com.scaper.app;
+
+/**
+ * Entry point for scraper application.
+ */
+public class ScraperApplication 
+{
+    public static void main( String[] args )
+    {
+        //Awaiting implementation
+    }
+}
