840e4891634ac9e9f44448bdd576a6e89a1f1c42
Matt Whitby <whitbymgt@gmail.com>
2019-05-12 16:52:23 +0100

    Implemented main in ScraperApplication to output JSON result

--- sainsburys-scraper/src/main/java/com/scaper/app/ScraperApplication.java
+++ sainsburys-scraper/src/main/java/com/scaper/app/ScraperApplication.java
@@ -1,12 +1,25 @@
 package com.scaper.app;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import com.scaper.utils.FoodProductJsonWriter;
+import com.scaper.utils.ProductsScraper;
+import com.scraper.constants.SainsburysBerriesHtmlQueries;
+
 /**
  * Entry point for scraper application.
  */
 public class ScraperApplication 
 {
+	private static final String URL = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+	
     public static void main( String[] args )
     {
-        //Awaiting implementation
+    	Connection connection = Jsoup.connect(URL);
+    	ProductsScraper scraper = new ProductsScraper(connection, new SainsburysBerriesHtmlQueries());
+    	
+    	FoodProductJsonWriter writer = new FoodProductJsonWriter(scraper.getProducts());
+    	writer.outputJsonResult();
     }
 }
